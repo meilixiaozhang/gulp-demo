f3df56d6296b2d56dca101d8c04597149cfff870
meilixiaozhang <redpangci@gmail.com>
2020-05-11 18:02:23 +1000

    add concat and watch

---
+++ README.md
@@ -0,0 +1,24 @@
+# Gulp Example/Starter App
+
+Example gulp application to show how to use plugins and run tasks
+
+## Dependencies
+gulp-imagemin
+gulp-concat
+gulp-uglify
+gulp-sass
+
+## Install Dependencies
+```bash
+npm install
+```
+
+## Run all tasks
+```bash
+gulp
+```
+
+## Watch all tasks/files
+```bash
+gulp watch
+```
--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require("gulp");
 const imagemin = require("gulp-imagemin")
 const uglify = require("gulp-uglify")
 const sass = require("gulp-sass")
+const concat = require("gulp-concat")
 
 // gulp.task - define tasks
 // gulp.src - points topfiles to use
@@ -10,35 +11,44 @@ const sass = require("gulp-sass")
 
 // logs message
 gulp.task('message', async () =>
-  console.log('gulp is running...')
-);
+  console.log('gulp is running...'));
 
 // copy all html files
 gulp.task('copyHTML', async () =>
   gulp.src("src/*.html")
-  .pipe(gulp.dest("dist"))
-);
+  .pipe(gulp.dest("dist")));
 
 // image optimization
 gulp.task('imageMin', async () =>
-	gulp.src('src/images/*')
-		.pipe(imagemin())
-		.pipe(gulp.dest('dist/images'))
-);
+  gulp.src('src/images/*')
+  .pipe(imagemin())
+  .pipe(gulp.dest('dist/images')));
 
 // minify js
 gulp.task('minify', async () =>
   gulp.src("src/js/*.js")
   .pipe(uglify())
-  .pipe(gulp.dest("dist/js"))
-);
+  .pipe(gulp.dest("dist/js")));
 
 // compile sass
 gulp.task('sass', async () =>
   gulp.src("src/sass/*.scss")
   .pipe(sass())
   .pipe(sass().on("error", sass.logError))
-  .pipe(gulp.dest("dist/css"))
-);
+  .pipe(gulp.dest("dist/css")));
 
-gulp.task('default',gulp.parallel(['message','copyHTML','imageMin','minify','sass']));
+// scripts
+gulp.task('scripts', async () =>
+  gulp.src("src/js/*.js")
+  .pipe(concat("main.js"))
+  .pipe(uglify())
+  .pipe(gulp.dest("dist/js")));
+
+gulp.task('default', gulp.parallel(['message', 'copyHTML', 'imageMin', 'sass', 'scripts']));
+
+gulp.task('watch', async()=>{
+    gulp.watch('src/js/*.js', gulp.series('scripts'));
+    gulp.watch('src/images/*', gulp.series('imageMin'));
+    gulp.watch('src/sass/*.scss', gulp.series('sass'));
+    gulp.watch('src/*.html', gulp.series('copyHTML'));
+});
